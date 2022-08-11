0955f211dd4574f8a1affc212cfcdfd6d0ed54e1
yamashita-tiemi <17800160777@estudante.ufjf.br>
2022-08-11 16:55:02 -0300

    finished layout

--- projeto3-driveneats/scripts.js
+++ projeto3-driveneats/scripts.js
@@ -1,3 +1,25 @@
-function item(img, ) {
-    
+function itemButton() {
+    //SELECIONAR AO CLICAR NO ITEM
+    let s = document.querySelector(".items");
+
+    // if () {
+    //     //DESMARCAR CO OUTRO SE JA ESTIVER SELECIONADO
+    // }
 }
+
+function finishButton() {
+    // OUTRA FRASE E BUTTON DESABILITADO
+    //ONCLIK = FALSE
+    let finishOFF = document.getElementById("finishOFF");
+    let finishON = document.getElementById("finishON");
+    finishOFF.hidden = false;
+    finishON.hidden = true;
+
+    if (cont == 3) {
+        // HABILITAR BUTTON DE FECHAR PEDIDO SE PEDIDOS =3
+        finishOFF.hidden = true;
+        finishON.hidden = false;
+        //document.getElementById(".finish").style.color = green;
+    }
+}
+
