543cf4e24192086e4ccb11258f7b3d24b8a59b1c
yamashita-tiemi <17800160777@estudante.ufjf.br>
2022-08-15 13:55:27 -0300

    implemented  Enable Button and functionality to Finish Button

--- projeto3-driveneats/scripts.js
+++ projeto3-driveneats/scripts.js
@@ -1,14 +1,12 @@
 
-
 function selectedItemButton(itemButton, categoryButton) {
     //SELECIONAR AO CLICAR NO ITEM
     const selectedItem = document.querySelector('.selected');
     const allItems = document.querySelectorAll('.selected');
-    let i = 0;
     
     //SE OUTRO ITEM DA MESMA CATEGORIA ESTIVER SELECIONADO, DESMARCAR-LO
     if(selectedItem != null ) {
-        for(i=0; i<allItems.length; i++) {
+        for(let i=0; i<allItems.length; i++) {
             if(allItems[i].classList.contains(categoryButton)) {
                 allItems[i].classList.remove('selected');
                 allItems[i].classList.remove('selectedIcon');
@@ -21,13 +19,13 @@ function selectedItemButton(itemButton, categoryButton) {
     itemButton.classList.add(categoryButton);
 
     //VERIFICA SE JA PODE FECHAR O PEDIDO
-    finishButton('.finish','.selected');
+    enableButton('.finish');
 }
 
-function finishButton(classFinish, selectedItem) {
+function enableButton(classFinish) {
     //FRASE INICIAL E BUTTON DESABILITADO
     const finish = document.querySelector(classFinish);
-    const allItems = document.querySelectorAll(selectedItem);
+    const allItems = document.querySelectorAll('.selected');
     // console.log(allItems); //"endereço"/caminho
     // console.log(allItems.length); //quantidade
 
@@ -38,4 +36,34 @@ function finishButton(classFinish, selectedItem) {
     }
     else
         finish.classList.remove('finishON');
+}
+
+function message() {
+    let str = "Olá, gostaria de fazer o pedido: \n";
+    let strAux = ["- Prato: ", "- Bebida: ", "- Sobremesa: "];
+    let aux1;
+    let aux2;
+    let price = [];
+    let priceAux;
+    let priceTotal = 0;
+    const allItems = document.querySelectorAll('.selected');
+    for(let i=0; i<allItems.length; i++) {
+        aux1 = allItems[i].children;
+        aux2 = aux1[1].children;
+        str += strAux[i] + aux2[0].innerHTML + '\n';
+        price[i] = aux2[2].innerHTML;
+        priceAux = price[i].replace(/[^\d,]/g, '');
+        // console.log(priceAux);
+        priceAux = priceAux.replace(',', '.');
+        // console.log(priceAux);
+        priceTotal += parseFloat(priceAux);
+    }
+    str += `Total: R$ ${priceTotal.toFixed(2)}`;
+    return str;
+}
+
+function finishButton() {
+    const messageURL = encodeURIComponent(message());
+    //concole.log(messageURL);
+    window.open(`https://wa.me/5524992946925?text=${messageURL}`);
 }
